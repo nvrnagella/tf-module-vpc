f3d111deff8a5344bbf05d27063199743eb8234a
IT1089778 <nvrnagella@gmail.com>
2023-07-07 22:42:03 +0530

    route table association for private subnet

--- main.tf
+++ main.tf
@@ -91,11 +91,11 @@ resource "aws_route_table_association" "private-rt-assoc" {
   subnet_id = aws_subnet.private.*.id[count.index]
   route_table_id = aws_route_table.private.id
 }
-#resource "aws_route" "r" {
-#  route_table_id = data.aws_vpc.default.main_route_table_id
-#  destination_cidr_block = var.cidr_block
-#  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-#}
+resource "aws_route" "r" {
+  route_table_id = data.aws_vpc.default.main_route_table_id
+  destination_cidr_block = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
 //Create EC2
 data "aws_ami" "centos8" {
   most_recent = true
