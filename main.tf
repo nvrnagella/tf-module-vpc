bb1b838d2cfc1e3ddece2be4666f4dfd7b8e7053
IT1089778 <nvrnagella@gmail.com>
2023-07-07 22:39:37 +0530

    route table association for private subnet

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ data "aws_ami" "centos8" {
 }
 resource "aws_instance" "web" {
   ami = data.aws_ami.centos8.id
-  instance_type = "t3.medium"
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   subnet_id = aws_subnet.private.*.id[0]
   tags = {
