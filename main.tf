15a01abc379c77d902940eb04a822d7d19ade292
IT1089778 <nvrnagella@gmail.com>
2023-07-24 13:40:01 +0530

    keeping availability zone to private subnets also

--- main.tf
+++ main.tf
@@ -132,21 +132,21 @@ resource "aws_security_group" "allow_tls" {
   ingress {
     description = "TLS from VPC"
     from_port = 22
-    protocol  = "tcp"
     to_port   = 22
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     description = "HTTP"
     from_port = 80
-    protocol  = "tcp"
     to_port   = 80
+    protocol  = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port = 0
-    protocol  = "-1"
     to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
