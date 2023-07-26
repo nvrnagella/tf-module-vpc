fb2afdcd0ce103da3530db8bdee5db404f25beb1
IT1089778 <nvrnagella@gmail.com>
2023-07-26 17:53:41 +0530

    outputs file added

--- subnets/main.tf
+++ subnets/main.tf
@@ -27,8 +27,8 @@ resource "aws_route_table_association" "association" {
   route_table_id    = aws_route_table.route_table.id
 }
 resource "aws_route" "internet_gateway_route" {
-  count                   = var.internet_gw == null ? 0 : 1
+  count                   = var.internet_gw ? 1 : 0
   route_table_id          = aws_route_table.route_table.id
   destination_cidr_block  = "0.0.0.0/0"
-  gateway_id              = var.internet_gw
+  gateway_id              = var.gateway_id
 }
--- subnets/vars.tf
+++ subnets/vars.tf
@@ -8,3 +8,4 @@ variable "vpc_peering_connection_id" {}
 variable "tags" {}
 variable "internet_gw" {}
 //variable "nat_gw" {}
+variable "gateway_id" {}
