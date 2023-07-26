db163d45f143c3651af2550647e708eab88ec73f
IT1089778 <nvrnagella@gmail.com>
2023-07-26 19:17:48 +0530

    public subnets sepereate

--- main.tf
+++ main.tf
@@ -29,3 +29,16 @@ resource "aws_internet_gateway" "gw" {
     { Name = "${var.env}-igw" }
   )
 }
+resource "aws_eip" "ngw-eip" {
+  vpc = true
+}
+resource "aws_nat_gateway" "ngw" {
+  count         = var.nat_gw ? 1 : 0
+  allocation_id = aws_eip.ngw-eip.id
+  subnet_id = var.public_subnets_ids[0]
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-ngw" }
+  )
+}
