bef4f2f3bad1ef3a037167c35c28aece1bba7b69
IT1089778 <nvrnagella@gmail.com>
2023-07-26 15:26:28 +0530

    comenting internet_gw and nag_gw for now

--- subnets.tf
+++ subnets.tf
@@ -8,8 +8,8 @@ module "subnets" {
   for_each    = var.subnets
   cidr_block  = each.value.cidr_block
   name        = each.value.name
-  internet_gw               = lookup(each.value, "internet_gw", false)
-  nat_gw                    = lookup(each.value, "nat_gw", false)
+#  internet_gw               = lookup(each.value, "internet_gw", false)
+#  nat_gw                    = lookup(each.value, "nat_gw", false)
 
   vpc_id                    = aws_vpc.main.id
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
