fbe14cc8f7ad05282f335c6c0630784fcb429420
IT1089778 <nvrnagella@gmail.com>
2023-07-26 19:54:32 +0530

    comenting out nat gateway we are not ready

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,5 @@ output "vpc_peering_connection_id" {
   value = aws_vpc_peering_connection.peer.id
 }
 output "public_subnet_ids" {
-  value = module.public_subnets.subnet_ids
+  value = module.public_subnets
 }
