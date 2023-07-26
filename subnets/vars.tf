fe26a7d9b5478cc8b6351bd9096982b472bdc43c
IT1089778 <nvrnagella@gmail.com>
2023-07-26 11:54:12 +0530

    included all subnets to vpc

---
+++ subnets/vars.tf
@@ -0,0 +1,11 @@
+variable "cidr_block" {}
+variable "availability_zone" {}
+variable "vpc_id" {}
+variable "env" {}
+variable "default_vpc_id" {}
+variable "name" {}
+variable "vpc_peering_connection_id" {}
+variable "tags" {}
+variable "internet_gw" {}
+variable "nat_gw" {}
+variable "create_nat_gw" {}
--- vars.tf
+++ vars.tf
@@ -1,6 +1,4 @@
 variable "cidr_block" {}
 variable "env" {}
-variable "public_subnets_cidr" {}
-variable "private_subnets_cidr" {}
 variable "default_vpc_id" {}
-variable "availability_zones" {}
+variable "subnets" {}
